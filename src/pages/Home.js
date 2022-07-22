b6fdb9942cdc3847eb6f5ee7c859b1a17b540c7f
Kevin Gwiazdowski <kgwiazdowski88@gmail.com>
2022-07-22 16:28:08 +0200

    Create SearchExercises and fetchData components

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,13 @@
 import React, { useState } from "react";
 import { Box } from "@mui/system";
 import Banner from "../components/Banner";
+import SearchExercises from "../components/SearchExercises";
 
 const Home = () => {
   return (
     <Box>
       <Banner />
+      <SearchExercises />
     </Box>
   );
 };
